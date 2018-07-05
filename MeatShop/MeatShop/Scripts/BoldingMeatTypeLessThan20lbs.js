0cb3c1670c7954e23d1e1a4165e1822e0736a507
lahari95 <lahari.surapaneni12@gmail.com>
2018-07-04 22:00:34 -0500

    Bolding Meat Type Less Than 20lbs Added

---
+++ MeatShop/MeatShop/Scripts/BoldingMeatTypeLessThan20lbs.js
@@ -0,0 +1,8 @@
+﻿var myElement = document.getElementsByClassName("meatRows");
+for (var i = 0; i < myElement.length; i++) {
+    var secondColumnElements = (myElement[i].getElementsByTagName("td")[1].innerText);
+    if (secondColumnElements < 20) {
+        myElement[i].style.color = "red";
+        myElement[i].style.fontWeight = "bold";
+    }
+}
